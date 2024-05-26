5d28e8d8709cc021e9246f9fa792c57d954f974a
kvnrynfl <kevinreynaufal2004@gmail.com>
2024-05-27 04:54:12 +0700

    feat: add index file for models

---
+++ models/index.js
@@ -0,0 +1,15 @@
+const User = require('./user');
+const Major = require('./major');
+const Course = require('./course');
+const Student = require('./student');
+const Lecturer = require('./lecturer');
+const Enrollment = require('./enrollment');
+
+module.exports = {
+    User,
+    Major,
+    Course,
+    Student,
+    Lecturer,
+    Enrollment
+}
