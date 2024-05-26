9e8bb56021a78a5041b3bb2eaf1bdc0871fac900
kvnrynfl <kevinreynaufal2004@gmail.com>
2024-05-27 04:50:46 +0700

    feat: add Student model schema

---
+++ models/student.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const studentSchema = new Schema({
+    user_id: {
+        type: Schema.Types.ObjectId,
+        ref: 'User', required: true
+    },
+    nim: {
+        type: String, required: true,
+        unique: true
+    },
+    major_id: {
+        type: Schema.Types.ObjectId,
+        ref: 'Major',
+        required: true
+    },
+    year_of_entry: {
+        type: Number,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    date_of_birth: {
+        type: Date,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['registered', 'verified', 'graduated', 'dropout'],
+        default: 'registered'
+    },
+    dropout_date: {
+        type: Date,
+        default: null
+    }
+}, {
+    timestamps: true
+});
+
+const Student = mongoose.model('Student', studentSchema);
+
+module.exports = Student;
