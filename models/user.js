35ea555f6df64154d64af51c9521f7fc47ae826e
kvnrynfl <kevinreynaufal2004@gmail.com>
2024-05-27 04:00:55 +0700

    feat: add User model schema

---
+++ models/user.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['admin', 'student', 'lecturer'],
+        required: true
+    },
+}, {
+    timestamps: true
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
