45a95dd4a48c0cff6d19d66a69cbd22d72622b4d
kvnrynfl <kevinreynaufal2004@gmail.com>
2024-05-28 03:31:55 +0700

    feat: integrate authentication routes

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
 const helmet = require('helmet');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+
+const authRoutes = require('./routes/authRoute');
 
 const PORT = process.env.PORT || 3000;
 
@@ -17,6 +19,8 @@ app.use(cors());
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
