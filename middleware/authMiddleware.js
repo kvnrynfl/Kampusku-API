af6cf6b52ff424ba5c20b3843bdda1117def0d42
kvnrynfl <kevinreynaufal2004@gmail.com>
2024-05-28 03:24:04 +0700

    feat: add authentication middleware

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,37 @@
+const { User } = require('../models');
+const { verifyToken } = require('../utils/jwt');
+
+const authMiddleware = async (req, res, next) => {
+    try {
+        const token = req.header('Authorization').replace('Bearer ', '');
+        if (!token) {
+            return res.status(401).json({
+                status: 'error',
+                message: 'Access denied. No token provided.',
+                data: null
+            });
+        }
+        const decoded = await verifyToken(token);
+        const user = await User.findOne({ _id: decoded._id });
+
+        if (!user) {
+            return res.status(401).json({
+                status: 'error',
+                message: 'Invalid token.',
+                data: null
+            });
+        }
+
+        req.token = token;
+        req.user = user;
+        next();
+    } catch (error) {
+        res.status(401).json({
+            status: 'error',
+            message: 'Invalid token.',
+            data: null
+        });
+    }
+};
+
+module.exports = authMiddleware
