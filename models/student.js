342fdf7ae08b0620b510339bb2bd06b0ba900ef2
kvnrynfl <kevinreynaufal2004@gmail.com>
2024-05-29 06:42:40 +0700

    feat: add pre-save hook to generate NIM in studentSchema

--- models/student.js
+++ models/student.js
@@ -20,6 +20,7 @@ const studentSchema = new Schema({
     },
     year_of_entry: {
         type: Number,
+        default: new Date().getFullYear(),
         required: true
     },
     phone: {
@@ -45,6 +46,15 @@ const studentSchema = new Schema({
     }
 }, {
     timestamps: true
+});
+
+studentSchema.pre('save', async function (next) {
+    if (this.isNew) {
+        const lastStudent = await mongoose.model('Student').findOne().sort({ createdAt: -1 });
+        const lastNim = lastStudent ? parseInt(lastStudent.nim, 10) : 0;
+        this.nim = (lastNim + 1).toString().padStart(8, '0');
+    }
+    next();
 });
 
 const Student = mongoose.model('Student', studentSchema);
