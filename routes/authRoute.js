3643fb9f0ea47130a3724c3743121374f2d10cce
kvnrynfl <kevinreynaufal2004@gmail.com>
2024-05-30 03:21:39 +0700

    feat: add check-token route to authRoute

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -7,6 +7,7 @@ const { loginValidators, changePasswordValidators, registerValidators } = requir
 router.post('/login', loginValidators, authController.login);
 router.post('/register', registerValidators, authController.register);
 router.get('/profile', authMiddleware, authController.profile);
+router.get('/check-token', authMiddleware, authController.checkToken);
 router.put('/change-password', authMiddleware, changePasswordValidators, authController.changePassword);
 
 module.exports = router;
